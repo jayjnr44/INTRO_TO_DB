67a791f0b5c1ce28f4595f380d36a7d31015c6e2
Felix Kuffour <Jayycrypt@gmail.com>
2025-08-14 15:51:08 +0000

    database made easy

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
 USE DATABASE alx_book_store;
-INSERT INTO TABLE Customers VALUES("1","Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave");
+INSERT INTO Customers VALUES("1","Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave");
